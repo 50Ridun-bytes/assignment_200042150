193946ce09e541247827f912316b0f621c83ba15
Tasrefee Mahmood Ridun <95172466+50Ridun-bytes@users.noreply.github.com>
2022-11-19 23:49:45 +0600

    Update StampCoupling.java

--- StampCoupling.java
+++ StampCoupling.java
@@ -1,1 +1,31 @@
+package assingment2;
+
+public class StampCouplingA {
+    private int[] array = {1, 2, 3, 4, 5};
+    StampCouplingC c = new StampCouplingC();
+    StampCouplingB b = new StampCouplingB();
+    public void start() {
+        b.callB(array, c);
+    }
+}
+
+public class StampCouplingB {
+    public void callB(int[] array, StampCouplingC c ) {
+        System.out.println("Array length: " + array.length);
+        System.out.println("Name: " + c.getName());
+    }
+}
+
+public class StampCouplingC {
+    private String name = "Ridun";
+    public String getName() {
+        return name;
+    }
+}
+/*
+ --- Explanation ---
+    Here StampCouplingA class is sending array and object c as parameters to StampCouplingB class. 
+    But StampCouplingB class is using the length of the array and the getName() method of class StampCouplingC. 
+    This is stamp coupling. This can be eliminated by passing only the necessary primitive types in the function call.
+ */
 
